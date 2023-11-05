642bf3515d7e75df70c1af884194752b5bd68c49
Alexander Stepanov <alexanderlawyer97@gmail.com>
2023-11-06 00:41:46 +0100

    Initial Commit

---
+++ HW-11.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-11.1
+//
+//  Created by Mac Alexander on 06.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
