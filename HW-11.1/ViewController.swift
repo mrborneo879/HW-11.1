326e30827f8205630b2065f12697c218a5cb7656
Alexander Stepanov <alexanderlawyer97@gmail.com>
2023-11-06 01:04:53 +0100

    Add BG imageView.

--- HW-11.1/ViewController.swift
+++ HW-11.1/ViewController.swift
@@ -1,13 +1,44 @@
 
 import UIKit
 
+// Я переделал название ViewController на First Screen, так как, по сути, у нас будет 1 экран, final class взято из видео для ДЗ, лектор говорил, что это хорошо для оптимизации.
 class FirstScreen: UIViewController {
 
+    // MARK: - UIElements/Oulets
+    
+    private lazy var imageView: UIImageView = {
+        let image = UIImage(named: "BGimage")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupHierarchy()
+        setupLayout()
     }
-
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        [imageView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setupLayout() {
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0)
+        ])
+    }
+    
+    // MARK: - Actions
 
 }
 
